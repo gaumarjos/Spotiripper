4d39027faea01b589f08d8c81bafce32de2d97f1
Stefano Salati <ste.salati@protonmail.com>
2021-11-13 20:15:22 +0100

    Spotipy variables

---
+++ set_env_variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "" >> ~/.zshrc
+echo "export LANG=\"en_US.UTF-8\"" >> ~/.zshrc
+echo "export SPOTIPY_CLIENT_ID='fac9a2b326fc492ab58611d32b2aae09'" >> ~/.zshrc
+echo "export SPOTIPY_CLIENT_SECRET='39f5a3a9f1ba4fb9b4992df531042fbb'" >> ~/.zshrc
