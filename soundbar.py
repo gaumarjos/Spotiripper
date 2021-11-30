801afaa323b547ba450e68d2e7414f345b00f920
Stefano Salati <ste.salati@protonmail.com>
2021-11-30 17:32:43 +0100

    Adding GUI support

--- soundbar.py
+++ soundbar.py
@@ -2,12 +2,13 @@ import numpy
 
 
 class SoundBar(object):
-    def __init__(self, limitx, graceperiod=20, terminal_width=80):
+    def __init__(self, limitx, graceperiod=20, terminal_width=80, gui_soundbar=None):
         self.limitx = limitx
         self.maxx = 0.0
         self.i = 0
         self.graceperiod = graceperiod
         self.w = terminal_width - 20
+        self.gui_soundbar = gui_soundbar
 
     def update(self, x):
         self.i = self.i + 1
@@ -21,8 +22,13 @@ class SoundBar(object):
             maxxbar = int(self.maxx / self.limitx * self.w)
             maxxbar = numpy.clip(maxxbar, a_min=0, a_max=self.w)
 
-            # print('|' + xbar * '\u2588' + (self.w - xbar) * ' ' + '| {:6.2f} {:6.2f}'.format(x, self.maxx), end='\r')
-            print('|' + xbar * '\u2588' + \
-                  (maxxbar - xbar) * ' ' + \
-                  '|' + (self.w - maxxbar - 1) * ' ' + \
-                  '| {:6.2f} {:6.2f}'.format(x, self.maxx), end='\r')
+            # Writing on terminal if no GUI is used or on GUI soundbar is GUI is used
+            if self.gui_soundbar is None:
+                # print('|' + xbar * '\u2588' + (self.w - xbar) * ' ' + '| {:6.2f} {:6.2f}'.format(x, self.maxx), end='\r')
+                print('|' + xbar * '\u2588' + \
+                      (maxxbar - xbar) * ' ' + \
+                      '|' + (self.w - maxxbar - 1) * ' ' + \
+                      '| {:6.2f} {:6.2f}'.format(x, self.maxx), end='\r')
+            else:
+                # In range 0-1
+                self.gui_soundbar.setValue(x / self.limitx)
