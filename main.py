f45c21557691f88086f24e321e1b9f072c702c08
Stefano Salati <ste.salati@protonmail.com>
2021-12-03 08:33:49 +0100

    Put ripper in different file for clarity

--- main.py
+++ main.py
@@ -7,31 +7,21 @@ https://stackoverflow.com/questions/53761033/pydub-play-audio-from-variable
 https://betterprogramming.pub/simple-audio-processing-in-python-with-pydub-c3a217dabf11
 '''
 
-import eyed3
 import os
-import shutil
-import subprocess
 import sys
-import time
 import traceback
 import settings_lib
-from urllib.request import urlopen
 import colorama
-# from converter_ffmpeg import convert_to_mp3
 import spotipy
 from PySide6.QtCore import Qt, QSize, QRunnable, Slot, Signal, QObject, QThreadPool
 from PySide6.QtGui import QSyntaxHighlighter, QTextCharFormat, QColor, QFont, QIcon, QAction
 from PySide6.QtWidgets import QApplication, QMainWindow, QTextEdit, QLineEdit, QPushButton, QSpinBox, QLabel, \
-    QProgressBar, QDialog, QDialogButtonBox, QVBoxLayout, QHBoxLayout, QGridLayout
+    QProgressBar, QDialog, QDialogButtonBox, QVBoxLayout, QGridLayout
 from spotipy.oauth2 import SpotifyClientCredentials
 
-from converter_pydub import convert_to_mp3
-# from recorder_pyaudio import Recorder
-from recorder_sounddevice import Recorder
+from ripper import Ripper
 
-# from plyer import notification
-
-VERSION = "2021-12-02"
+VERSION = "2021-12-03"
 DRYRUN = False
 
 MACOSRED = (236, 95, 93)
@@ -65,21 +55,6 @@ def help():
     print("    export SPOTIPY_CLIENT_ID='yourclientid'")
     print("    export SPOTIPY_CLIENT_SECRET='yourclientsecret'")
     return 1
-
-
-def push(title, message):
-    subprocess.Popen(
-        'osascript -e "display notification \\"' + message + '\\" with title \\"' + title + '\\""',
-        shell=True, stdout=subprocess.PIPE).stdout.read()
-
-    '''
-    notification.notify(
-        title=title,
-        message=message,
-        app_icon="spotiripper.png",
-        timeout=50
-    )
-    '''
 
 
 def convert_to_uri(s):
@@ -199,13 +174,6 @@ def parse_input(link, start_from=0):
         return tracks[start_from:], error
 
 
-def remove_bad_characters(s):
-    bad_chars = [';', ':', '!', '*', '/', '\\']
-    for i in bad_chars:
-        s = s.replace(i, '')
-    return s
-
-
 def resource_path(path):
     # Needed only is the path is relative
     if not os.path.isabs(path):
@@ -214,163 +182,6 @@ def resource_path(path):
         return os.path.join(os.path.abspath('.'), path)
     else:
         return path
-
-
-'''
-Ripper
-'''
-
-
-class Ripper:
-    def __init__(self,
-                 rip_dir=os.path.expanduser('~') + '/Downloads/Ripped/',
-                 ripped_folder_structure='flat',
-                 gui=False,
-                 gui_progress_callback=None,
-                 gui_soundbar_callback=None):
-        self.ripped_folder_structure = ripped_folder_structure
-        self.rip_dir = rip_dir
-        self.tmp_dir = self.rip_dir + '.tmp/'
-        self.tmp_wav = "tmp.wav"
-        self.tmp_mp3 = "tmp.mp3"
-        self.recfile = None
-        # self.converter = "ffmpeg"
-        self.converter = "pydub"
-
-        # Create directory for the ripped tracks
-        if not os.path.exists(self.rip_dir):
-            os.makedirs(self.rip_dir)
-        if not os.path.exists(self.tmp_dir):
-            os.makedirs(self.tmp_dir)
-
-        # Determine width of terminal window to fit soundbar
-        self.terminal_width = shutil.get_terminal_size((80, 20)).columns
-
-        # Taking GUI elements as parameters in order to write on them
-        self.gui = gui
-        self.gui_progress_callback = gui_progress_callback
-        self.gui_soundbar_callback = gui_soundbar_callback
-
-    def rip(self, track):
-        toprint = "Ripping track {}...".format(track)
-        if self.gui:
-            self.gui_progress_callback.emit(toprint)
-        else:
-            print(toprint + (self.terminal_width - len(toprint)) * ' ')
-
-        # Clear all previous recordings if they exist
-        for f in os.listdir(self.tmp_dir):
-            os.remove(os.path.join(self.tmp_dir, f))
-
-        # Tell Spotify to pause
-        subprocess.Popen('osascript -e "tell application \\"Spotify\\" to pause"', shell=True,
-                         stdout=subprocess.PIPE).stdout.read()
-        time.sleep(.300)
-
-        # Start recorder
-        self.recfile = Recorder(self.tmp_dir + self.tmp_wav,
-                                terminal_width=self.terminal_width,
-                                gui=self.gui,
-                                gui_progress_callback=self.gui_progress_callback,
-                                gui_soundbar_callback=self.gui_soundbar_callback)
-        # self.recfile.getinfo()
-
-        self.recfile.start_recording()
-
-        # Tell Spotify to play a specified song
-        subprocess.Popen(
-            'osascript -e "tell application \\"Spotify\\"" -e "play track \\"' + track + '\\"" -e "end tell"',
-            shell=True, stdout=subprocess.PIPE).stdout.read()
-
-        time.sleep(1)
-
-        # Get the artist name, track name, album name and album artwork URL from Spotify
-        artist = subprocess.Popen(
-            'osascript -e "tell application \\"Spotify\\"" -e "current track\'s artist" -e "end tell"', shell=True,
-            stdout=subprocess.PIPE).stdout.read().decode('utf-8').rstrip('\r\n')
-        track = subprocess.Popen(
-            'osascript -e "tell application \\"Spotify\\"" -e "current track\'s name" -e "end tell"',
-            shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8').rstrip('\r\n')
-        album = subprocess.Popen(
-            'osascript -e "tell application \\"Spotify\\"" -e "current track\'s album" -e "end tell"',
-            shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8').rstrip('\r\n')
-        artwork = subprocess.Popen(
-            'osascript -e "tell application \\"Spotify\\"" -e "current track\'s artwork url" -e "end tell"', shell=True,
-            stdout=subprocess.PIPE).stdout.read().decode('utf-8').rstrip('\r\n')
-        artworkdata = urlopen(artwork).read()
-
-        # Print artist and track names extracted from Spotify
-        toprint = "Track: {}, {}".format(artist, track)
-        if self.gui:
-            self.gui_progress_callback.emit(toprint)
-        else:
-            try:
-                print(colorama.Back.LIGHTGREEN_EX,
-                      toprint + (self.terminal_width - len(toprint) - 6) * ' ',
-                      colorama.Style.RESET_ALL)
-                push("Now recording...", toprint)
-            except:
-                toprint = toprint.encode('ascii', 'replace')
-                print(colorama.Back.LIGHTGREEN_EX,
-                      toprint + (self.terminal_width - len(toprint) - 6) * ' ',
-                      colorama.Style.RESET_ALL)
-                print(colorama.Fore.LIGHTYELLOW_EX,
-                      "The song name contains accented characters that cannot be displayed correctly. Make sure you set \"export PYTHONIOENCODING=utf-8\".",
-                      colorama.Style.RESET_ALL)
-
-        # Check every 500ms if Spotify has stopped playing
-        while subprocess.Popen('osascript -e "tell application \\"Spotify\\"" -e "player state" -e "end tell"',
-                               shell=True,
-                               stdout=subprocess.PIPE).stdout.read() == b"playing\n":
-            time.sleep(.500)
-
-        # Spotify has stopped playing, stop recording
-        self.recfile.stop_recording()
-
-        # Convert to mp3 and notify if an error occurs
-        if convert_to_mp3(self.tmp_dir + self.tmp_wav,
-                          self.tmp_dir + self.tmp_mp3):
-            push("Finished", toprint)
-        else:
-            toprint = "Error in conversion."
-            if self.gui:
-                self.gui_progress_callback.emit(toprint)
-            else:
-                print(toprint)
-
-        time.sleep(.500)
-
-        # Set and/or update ID3 information
-        mp3file = eyed3.load(self.tmp_dir + self.tmp_mp3)
-        mp3file.tag.images.set(3, artworkdata, "image/jpeg", track)
-        mp3file.tag.artist = artist
-        mp3file.tag.album = album
-        mp3file.tag.title = track
-        mp3file.tag.save()
-        destination_filename = self.rip_dir + remove_bad_characters(artist) + ", " + remove_bad_characters(
-            album) + ", " + remove_bad_characters(track) + ".mp3"
-
-        # Move to final location
-        if self.ripped_folder_structure == "flat":
-            for f in os.listdir(self.tmp_dir):
-                if f.endswith(".mp3"):
-                    shutil.move(self.tmp_dir + f,
-                                destination_filename)
-
-        elif self.ripped_folder_structure == "tree":
-            # Create directory for the artist
-            if not os.path.exists(self.rip_dir + artist):
-                os.makedirs(self.rip_dir + artist)
-
-            # Create directory for the album
-            if not os.path.exists(self.rip_dir + artist + "/" + album):
-                os.makedirs(self.rip_dir + artist + "/" + album)
-
-            # Move MP3 file to the folder containing rips.
-            for f in os.listdir(self.tmp_dir):
-                if f.endswith(".mp3"):
-                    shutil.move(self.tmp_dir + f,
-                                self.rip_dir + artist + "/" + album + "/" + track + ".mp3")
 
 
 '''
@@ -701,7 +512,8 @@ def main_gui():
                 grid_layout.addWidget(spotipy_client_secret_edit, 2, 1)
 
                 spotipy_client_info_label = QLabel(self)
-                spotipy_client_info_label.setText("If left empty, env. variables SPOTIPY_CLIENT_ID\nand SPOTIPY_CLIENT_SECRET will be used.")
+                spotipy_client_info_label.setText(
+                    "If left empty, env. variables SPOTIPY_CLIENT_ID\nand SPOTIPY_CLIENT_SECRET will be used.")
                 spotipy_client_info_label.setFixedHeight(50)
                 grid_layout.addWidget(spotipy_client_info_label, 3, 1)
 
