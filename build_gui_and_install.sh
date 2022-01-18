cd90ca753515ff08de2325a24e9b8897504bc09f
Stefano Salati <ste.salati@protonmail.com>
2022-01-18 19:16:04 +0100

    Trying 3.10 to build

--- build_gui_and_install.sh
+++ build_gui_and_install.sh
@@ -1,5 +1,6 @@
 # Remember to install pyenv Python with
 # env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install 3.9.7
+# env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.10.0
 
 # Build
 clear
@@ -9,7 +10,6 @@ pyinstaller --noconfirm --log-level=ERROR \
     --onefile \
     --windowed \
     --name Spotiripper \
-    --paths ~/.pyenv/versions/spotiripper/ \
     --paths ~/.pyenv/versions/3.9.7/envs/spotiripper/lib/python3.9/site-packages/ \
     --add-data settings.json:. \
     --add-data spotiripper.png:. \
@@ -22,6 +22,7 @@ pyinstaller --noconfirm --log-level=ERROR \
     spotiripper.py
 
 # --hidden-import plyer.platforms.macosx.notification \
+# --paths ~/.pyenv/versions/spotiripper/ \
 
 # Install
 cp -R dist/Spotiripper.app /Applications/
